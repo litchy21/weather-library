67694406d3ef0ff0238fa27f3f85dd5c1c9aaf9b
Louise <louise.roussel@epitech.eu>
2018-07-14 13:51:18 +0200

    Add annotations

--- src/Litchy21/WeatherLibrary/Weather.php
+++ src/Litchy21/WeatherLibrary/Weather.php
@@ -4,15 +4,26 @@ namespace Litchy21\WeatherLibrary;
 
 use GuzzleHttp\Client;
 
+/**
+ * Class Weather
+ * @package Litchy21\WeatherLibrary
+ */
 class Weather
 {
     private $apiKey = '2021a1fdc0c0625b69ae1f01727e6512';
 
+    /**
+     * Weather constructor.
+     * @param $apiKey
+     */
     public function __construct($apiKey)
     {
         $this->apiKey = $apiKey;
     }
 
+    /**
+     * @return array
+     */
     public function getWeather()
     {
         $client = new Client();
